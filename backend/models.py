441467fc7ee11de930b7e960ef884db60a0f2ac6
IlinDenis <denni-dog@mail.ru>
2025-01-13 22:31:35 +0300

    Add new 3 models for DB: Shop, Category, Product

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+""" 
+The file describes the application's DB models,
+each class has an ovveride of the base method __str__
+"""
+class Shop(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    shops = models.ManyToManyField(Shop, related_name='categories')
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
