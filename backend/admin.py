4875639f89f42b0d66785fc8c141eb68e1812339
IlinDenis <denni-dog@mail.ru>
2025-01-15 14:08:29 +0300

    Register addable lines in admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Shop, Product, ProductInfo, Category
 
-# Register your models here.
+
+class ProductInfoInline(admin.TabularInline):
+    model = ProductInfo
+    extra = 1 # Количество добавляемых пустых форм новых характеристик
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductInfoInline]
+
+
+admin.site.register(Shop)
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductInfo)
