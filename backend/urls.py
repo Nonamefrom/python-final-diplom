ea4fc437ab5107b6717bc1e5f413ed74eabf3cce
IlinDenis <denni-dog@mail.ru>
2025-01-20 21:39:52 +0300

    Add url's, one for register user, second for confirmation registration

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from backend.views import LoginView
+from backend.views import LoginView, RegisterAccountView, ConfirmEmailView
 
 
+app_name = 'backend'
 """
 Urlpatterns for backend api
 Урлпаттерны для api приложения backend
 """
-app_name = 'backend'
 urlpatterns = [
-    path('login', LoginView.as_view(), name='login')
+    path('user/login', LoginView.as_view(), name='login'),
+    path('user/register', RegisterAccountView.as_view(), name='user-register'),
+    path('confirm-email/<int:user_id>/', ConfirmEmailView.as_view(), name='confirm-email'),
     ]
