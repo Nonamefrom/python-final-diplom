6693feb4f30a027713455eed37c59f6e6626ca59
IlinDenis <denni-dog@mail.ru>
2025-01-24 21:00:29 +0300

    Add new url for catalog, with filters by get-parameter's in url, examples in file api_examples_for_testing.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from backend.views import LoginView, RegisterAccountView, ConfirmEmailView
+from backend.views import LoginView, RegisterAccountView, ConfirmEmailView, ProductInfoView
 
 
 app_name = 'backend'
@@ -11,4 +11,5 @@ urlpatterns = [
     path('user/login', LoginView.as_view(), name='login'),
     path('user/register', RegisterAccountView.as_view(), name='user-register'),
     path('confirm-email/<int:user_id>/', ConfirmEmailView.as_view(), name='confirm-email'),
+    path('products/', ProductInfoView.as_view(), name='product-list'),
     ]
