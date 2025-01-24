1e34a2a4e1e0da484c52dbf04090bdba3e683ae4
IlinDenis <denni-dog@mail.ru>
2025-01-24 21:28:01 +0300

    Add new serialaizer's for get catalog method.

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
+from .models import Product, ProductParameter, ProductInfo
 
 
 class LoginSerializer(serializers.Serializer):
@@ -14,9 +15,9 @@ class LoginSerializer(serializers.Serializer):
 
 class RegisterAccountSerializer(serializers.Serializer):
     """
-        Serializer for registrarion post method, validate mail and password.
-        Сериалайзер для регистрации post method, валидирующий мейл и пароль.
-        """
+    Serializer for registrarion post method, validate mail and password.
+    Сериалайзер для регистрации post method, валидирующий мейл и пароль.
+    """
     name = serializers.CharField(max_length=50)
     surname = serializers.CharField(max_length=50)
     email = serializers.EmailField()
@@ -37,3 +38,41 @@ class RegisterAccountSerializer(serializers.Serializer):
             is_active=False  # Set false until email confirmation, false пока юзер не подтвердил регистрацию
         )
         return user
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    """
+    Serializer for catalog get, get name and category.
+    Сериалайзер для catalog get, берет имя и категорию.
+    """
+    category = serializers.StringRelatedField()
+
+    class Meta:
+        model = Product
+        fields = ('name', 'category',)
+
+
+class ProductParameterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for catalog get, get parameter and value.
+    Сериалайзер для catalog get, берет параметр и значение.
+    """
+    parameter = serializers.StringRelatedField()
+
+    class Meta:
+        model = ProductParameter
+        fields = ('parameter', 'value',)
+
+
+class ProductInfoSerializer(serializers.ModelSerializer):
+    """
+    Serializer for catalog get, get parameters productinfo.
+    Сериалайзер для catalog get, берет параметры productinfo.
+    """
+    product = ProductSerializer(read_only=True)
+    product_parameters = ProductParameterSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = ProductInfo
+        fields = ('id', 'model', 'product', 'shop', 'quantity', 'price', 'price_rrc', 'product_parameters',)
+        read_only_fields = ('id',)
