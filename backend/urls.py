9c0026ad7bbabb69995e55af11c7e9850e707f4a
IlinDenis <denni-dog@mail.ru>
2025-01-27 22:53:23 +0300

    Refactor same FAQ info, register route for Contacts api's methods.

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from backend.views import LoginView, RegisterAccountView, ConfirmEmailView, ProductInfoView, BasketViewSet
+from backend.views import (LoginView, RegisterAccountView, ConfirmEmailView, ProductInfoView, BasketViewSet,
+                           ContactViewSet)
 
 
 app_name = 'backend'
 router = DefaultRouter()
-router.register(r'basket', BasketViewSet, basename='basket')
+router.register(r'basket', BasketViewSet, basename='basket') # Route for basketApi, роут для корзины
+router.register(r'contacts', ContactViewSet, basename='contact') # Route for contactApi, роут для контактов
 """
 Urlpatterns for backend api
 Урлпаттерны для api приложения backend
@@ -15,5 +17,5 @@ urlpatterns = [
     path('user/register', RegisterAccountView.as_view(), name='user-register'),
     path('confirm-email/<int:user_id>/', ConfirmEmailView.as_view(), name='confirm-email'),
     path('products/', ProductInfoView.as_view(), name='product-list'),
-    path('', include(router.urls)), # Add route for basketviewset, добавляем роуты для basketviewset
+    path('', include(router.urls)), # Add route for viewset, добавляем роуты для viewset
     ]
