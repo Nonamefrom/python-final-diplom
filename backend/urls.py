1720e1e40b063e08d8a4e10ac7a72c396aa0f7d0
IlinDenis <denni-dog@mail.ru>
2025-01-27 13:28:06 +0300

    Add url-path for basket API's

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from backend.views import LoginView, RegisterAccountView, ConfirmEmailView, ProductInfoView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from backend.views import LoginView, RegisterAccountView, ConfirmEmailView, ProductInfoView, BasketViewSet
 
 
 app_name = 'backend'
+router = DefaultRouter()
+router.register(r'basket', BasketViewSet, basename='basket')
 """
 Urlpatterns for backend api
 Урлпаттерны для api приложения backend
@@ -12,4 +15,5 @@ urlpatterns = [
     path('user/register', RegisterAccountView.as_view(), name='user-register'),
     path('confirm-email/<int:user_id>/', ConfirmEmailView.as_view(), name='confirm-email'),
     path('products/', ProductInfoView.as_view(), name='product-list'),
+    path('', include(router.urls)), # Add route for basketviewset, добавляем роуты для basketviewset
     ]
