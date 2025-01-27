9428619f6109f7e7e9275a212474ce99368952a5
IlinDenis <denni-dog@mail.ru>
2025-01-27 13:27:25 +0300

    Add settings for use method's, which need authtoken

--- orders/settings.py
+++ orders/settings.py
@@ -145,4 +145,21 @@ EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'noreply@localhost'
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 40,
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+
+    ),
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+
+}
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
