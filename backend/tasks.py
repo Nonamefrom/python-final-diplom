91ebf84f4ab7b3b5d101808c3c4e493ea66408a2
IlinDenis <denni-dog@mail.ru>
2025-02-10 14:19:42 +0300

    add tasks for download pictures

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,5 +1,8 @@
 from celery import shared_task
 from django.core.mail import send_mail
+from easy_thumbnails.files import generate_all_aliases
+from django.core.files.storage import default_storage
+import os
 
 
 @shared_task
@@ -19,3 +22,20 @@ def send_order_confirmation_email(order_id, email, total_price, city, street):
         recipient_list=[email],
         fail_silently=False,
     )
+
+
+@shared_task
+def process_product_image(image_path):
+    """Создание миниатюр для загруженного изображения товара"""
+    if default_storage.exists(image_path):
+        generate_all_aliases(image_path, include_global=True)
+        return f"Миниатюры созданы для {image_path}"
+    return f"Файл {image_path} не найден"
+
+@shared_task
+def process_user_avatar(image_path):
+    """Создание миниатюр для аватара пользователя"""
+    if default_storage.exists(image_path):
+        generate_all_aliases(image_path, include_global=True)
+        return f"Миниатюры созданы для {image_path}"
+    return f"Файл {image_path} не найден"
