3ca5e712dc4f01b179182629847ecfe699b1c09d
IlinDenis <denni-dog@mail.ru>
2025-02-15 22:28:37 +0300

    Add url for auth by github

--- orders/urls.py
+++ orders/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('auth/', include('social_django.urls', namespace='social')),
     path('jet/', include('jet.urls', 'jet')),
 ]
